6a72066691123ca3c0eabad5f8e64202a9b62888
CZC91 <karlosz.91@gmail.com>
2015-07-27 11:02:36 -0430

    Updated astroidal-ellipsoid.js

--- src/objects/primitives/astroidal-ellipsoid.js
+++ src/objects/primitives/astroidal-ellipsoid.js
@@ -1,10 +1,10 @@
-EZ3.ASTROIDAL_ELLIPSOID = function(xRadius, yRadius, zRadius, stacks, slices) {
+EZ3.ASTROIDAL_ELLIPSOID = function(radiusx, radiusy, radiusz, stacks, slices) {
 
-  this.slices = slices;
-  this.stacks = stacks;
-  this.xRadius = xRadius;
-  this.yRadius = yRadius;
-  this.zRadius = zRadius;
+  this._slices = slices;
+  this._stacks = stacks;
+  this._radiusx = radiusx;
+  this._radiusy = radiusy;
+  this._radiusz = radiusz;
 
   this.uv = [];
   this.indices = [];
@@ -26,11 +26,11 @@ EZ3.ASTROIDAL_ELLIPSOID.prototype.create = function() {
   vertex = vec3.create();
   normal = vec3.create();
 
-  S = 1.0 / (this.slices - 1);
-  T = 1.0 / (this.stacks - 1);
+  S = 1.0 / (this._slices - 1);
+  T = 1.0 / (this._stacks - 1);
 
-  for(s = 0; s < this.slices; ++s) {
-    for(t = 0; t < this.stacks; ++t) {
+  for(s = 0; s < this._slices; ++s) {
+    for(t = 0; t < this._stacks; ++t) {
 
       u = s * S;
       v = t * T;
@@ -43,13 +43,13 @@ EZ3.ASTROIDAL_ELLIPSOID.prototype.create = function() {
       sinS = Math.pow(Math.sin(phi), 3.0);
       sinT = Math.pow(Math.sin(rho), 3.0);
 
-      vertex[0] = (this.xRadius * cosT * cosS);
-      vertex[1] = (this.yRadius * sinT);
-      vertex[2] = (this.zRadius * cosT * sinS);
+      vertex[0] = (this._radiusx * cosT * cosS);
+      vertex[1] = (this._radiusy * sinT);
+      vertex[2] = (this._radiusz * cosT * sinS);
 
-      normal[0] = vertex[0] / this.xRadius;
-      normal[1] = vertex[1] / this.yRadius;
-      normal[2] = vertex[2] / this.zRadius;
+      normal[0] = vertex[0] / this._xradius;
+      normal[1] = vertex[1] / this._yradius;
+      normal[2] = vertex[2] / this._zradius;
 
       vec3.normalize(normal, normal);
 
@@ -67,18 +67,17 @@ EZ3.ASTROIDAL_ELLIPSOID.prototype.create = function() {
     }
   }
 
-  for(s = 0; s < this.slices - 1; ++s) {
-    for(t = 0; t < this.stacks - 1; ++t) {
+  for(s = 0; s < this._slices - 1; ++s) {
+    for(t = 0; t < this._stacks - 1; ++t) {
 
-      this.indices.push((s + 0) * this.stacks + (t + 0));
-      this.indices.push((s + 0) * this.stacks + (t + 1));
-      this.indices.push((s + 1) * this.stacks + (t + 1));
+      this.indices.push((s + 0) * this._stacks + (t + 0));
+      this.indices.push((s + 0) * this._stacks + (t + 1));
+      this.indices.push((s + 1) * this._stacks + (t + 1));
 
-      this.indices.push((s + 0) * this.stacks + (t + 0));
-      this.indices.push((s + 1) * this.stacks + (t + 1));
-      this.indices.push((s + 1) * this.stacks + (t + 0));
+      this.indices.push((s + 0) * this._stacks + (t + 0));
+      this.indices.push((s + 1) * this._stacks + (t + 1));
+      this.indices.push((s + 1) * this._stacks + (t + 0));
 
     }
   }
-
 };
