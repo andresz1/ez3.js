35ddc4139464e2b1aebdcff590e1d1ee2c95dfaf
CARLOS ZAPATA <carlos.zapata.conforto@gmail.com>
2015-07-30 20:47:11 -0430

    Updated Ez3.js

--- src/Ez3.js
+++ src/Ez3.js
@@ -1,11 +1,1 @@
-var EZ3 = {
-  VERSION: '1.0.0',
-
-  Vec2: vec2,
-  Vec3: vec3,
-  Vec4: vec4,
-  Mat2: mat2,
-  Mat3: mat3,
-  Mat4: mat4,
-  Signal: signals.Signal
-};
+var EZ3 = { VERSION: '1.0.0' };
