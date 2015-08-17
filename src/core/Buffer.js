5375b7abf35d30057bffe65b5975361cae07cf3e
CARLOS ZAPATA <carlos.zapata.conforto@gmail.com>
2015-08-16 22:19:04 -0430

    Updated buffer.js to dynamic mode

--- src/core/Buffer.js
+++ src/core/Buffer.js
@@ -1,8 +1,5 @@
 EZ3.Buffer = function() {
   this._buffer = [];
-  this._bufferSize = {};
-  this._bufferData = {};
-
   this._buffer[EZ3.Buffer.UV] = -1;
   this._buffer[EZ3.Buffer.INDEX] = -1;
   this._buffer[EZ3.Buffer.VERTEX] = -1;
@@ -30,92 +27,89 @@ EZ3.Buffer.UV_SIZE = 2;
 EZ3.Buffer.TANGENT_SIZE = 4;
 EZ3.Buffer.BITANGENT_SIZE = 3;
 
-EZ3.Buffer.prototype.fill = function(buffer, size, data) {
-  this._bufferData[buffer] = data;
-  this._bufferSize[buffer] = size;
-};
+EZ3.Buffer.prototype.draw = function(gl, draw, size) {
 
-EZ3.Buffer.prototype.draw = function(gl) {
-
-  gl.bindBuffer(gl.ARRAY_BUFFER, this._buffer[EZ3.Buffer.VERTEX]);
-  gl.enableVertexAttribArray(EZ3.Buffer.VERTEX_LAYOUT);
-  gl.vertexAttribPointer(EZ3.Buffer.VERTEX_LAYOUT, EZ3.Buffer.VERTEX_SIZE, gl.FLOAT, false, 0, 0);
-
-  if(this._bufferSize[EZ3.Buffer.NORMAL]) {
-    gl.bindBuffer(gl.ARRAY_BUFFER, this._buffer[EZ3.Buffer.NORMAL]);
-    gl.enableVertexAttribArray(EZ3.Buffer.NORMAL_LAYOUT);
-    gl.vertexAttribPointer(EZ3.Buffer.NORMAL_LAYOUT, EZ3.Buffer.NORMAL_SIZE, gl.FLOAT, false, 0, 0);
-  }
-
-  if(this._bufferSize[EZ3.Buffer.UV]) {
-    gl.bindBuffer(gl.ARRAY_BUFFER, this._buffer[EZ3.Buffer.UV]);
-    gl.enableVertexAttribArray(EZ3.Buffer.UV_LAYOUT);
-    gl.vertexAttribPointer(EZ3.Buffer.UV_LAYOUT, EZ3.Buffer.UV_SIZE, gl.FLOAT, false, 0, 0);
-  }
-
-  if(this._bufferSize[EZ3.Buffer.TANGENT]) {
-    gl.bindBuffer(gl.ARRAY_BUFFER, this._buffer[EZ3.Buffer.TANGENT]);
-    gl.enableVertexAttribArray(EZ3.Buffer.TANGENT_LAYOUT);
-    gl.vertexAttribPointer(EZ3.Buffer.TANGENT_LAYOUT, EZ3.Buffer.TANGENT_SIZE, gl.FLOAT, false, 0, 0);
-  }
-
-  if(this._bufferSize[EZ3.Buffer.BITANGENT]) {
+  if(~this._buffer[EZ3.Buffer.BITANGENT]) {
     gl.bindBuffer(gl.ARRAY_BUFFER, this._buffer[EZ3.Buffer.BITANGENT]);
     gl.enableVertexAttribArray(EZ3.Buffer.BITANGENT_LAYOUT);
     gl.vertexAttribPointer(EZ3.Buffer.BITANGENT_LAYOUT, EZ3.Buffer.BITANGENT_SIZE, gl.FLOAT, false, 0, 0);
   }
 
-  if(this._bufferSize[EZ3.Buffer.INDEX]){
+  if(~this._buffer[EZ3.Buffer.TANGENT]) {
+    gl.bindBuffer(gl.ARRAY_BUFFER, this._buffer[EZ3.Buffer.TANGENT]);
+    gl.enableVertexAttribArray(EZ3.Buffer.TANGENT_LAYOUT);
+    gl.vertexAttribPointer(EZ3.Buffer.TANGENT_LAYOUT, EZ3.Buffer.TANGENT_SIZE, gl.FLOAT, false, 0, 0);
+  }
+
+  if(~this._buffer[EZ3.Buffer.UV]) {
+    gl.bindBuffer(gl.ARRAY_BUFFER, this._buffer[EZ3.Buffer.UV]);
+    gl.enableVertexAttribArray(EZ3.Buffer.UV_LAYOUT);
+    gl.vertexAttribPointer(EZ3.Buffer.UV_LAYOUT, EZ3.Buffer.UV_SIZE, gl.FLOAT, false, 0, 0);
+  }
+
+  if(~this._buffer[EZ3.Buffer.NORMAL]) {
+    gl.bindBuffer(gl.ARRAY_BUFFER, this._buffer[EZ3.Buffer.NORMAL]);
+    gl.enableVertexAttribArray(EZ3.Buffer.NORMAL_LAYOUT);
+    gl.vertexAttribPointer(EZ3.Buffer.NORMAL_LAYOUT, EZ3.Buffer.NORMAL_SIZE, gl.FLOAT, false, 0, 0);
+  }
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, this._buffer[EZ3.Buffer.VERTEX]);
+  gl.enableVertexAttribArray(EZ3.Buffer.VERTEX_LAYOUT);
+  gl.vertexAttribPointer(EZ3.Buffer.VERTEX_LAYOUT, EZ3.Buffer.VERTEX_SIZE, gl.FLOAT, false, 0, 0);
+
+  if(draw === EZ3.ELEMENTS_DRAW) {
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this._buffer[EZ3.Buffer.INDEX]);
-    gl.drawElements(gl.TRIANGLES, this._bufferSize[EZ3.Buffer.INDEX], gl.UNSIGNED_SHORT, 0);
+    gl.drawElements(gl.TRIANGLES, size, gl.UNSIGNED_SHORT, 0);
   }else{
-    gl.drawArrays(gl.TRIANGLES, 0, this._bufferSize[EZ3.Buffer.VERTEX] / 3);
+    gl.drawArrays(gl.TRIANGLES, 0, size / 3);
   }
 
 };
 
-EZ3.Buffer.prototype.init = function(gl) {
+EZ3.Buffer.prototype.init = function(gl, id, size, data, hint) {
 
-  if(this._bufferSize[EZ3.Buffer.VERTEX]) {
+  if(id === EZ3.Buffer.VERTEX){
+
     this._buffer[EZ3.Buffer.VERTEX] = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, this._buffer[EZ3.Buffer.VERTEX]);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this._bufferData[EZ3.Buffer.VERTEX]), gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), hint);
     gl.bindBuffer(gl.ARRAY_BUFFER, null);
-  }
 
-  if(this._bufferSize[EZ3.Buffer.NORMAL]) {
+  }else if(id === EZ3.Buffer.NORMAL){
+
     this._buffer[EZ3.Buffer.NORMAL] = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, this._buffer[EZ3.Buffer.NORMAL]);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this._bufferData[EZ3.Buffer.NORMAL]), gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), hint);
     gl.bindBuffer(gl.ARRAY_BUFFER, null);
-  }
 
-  if(this._bufferSize[EZ3.Buffer.INDEX]) {
-    this._buffer[EZ3.Buffer.INDEX] = gl.createBuffer();
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this._buffer[EZ3.Buffer.INDEX]);
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(this._bufferData[EZ3.Buffer.INDEX]), gl.STATIC_DRAW);
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
-  }
+  }else if(id === EZ3.Buffer.UV){
 
-  if(this._bufferSize[EZ3.Buffer.UV]) {
     this._buffer[EZ3.Buffer.UV] = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, this._buffer[EZ3.Buffer.UV]);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this._bufferData[EZ3.Buffer.UV]), gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), hint);
     gl.bindBuffer(gl.ARRAY_BUFFER, null);
-  }
 
-  if(this._bufferSize[EZ3.Buffer.TANGENT]) {
+  }else if(id === EZ3.Buffer.TANGENT){
+
     this._buffer[EZ3.Buffer.TANGENT] = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, this._buffer[EZ3.Buffer.TANGENT]);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this._bufferData[EZ3.Buffer.TANGENT]), gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), hint);
     gl.bindBuffer(gl.ARRAY_BUFFER, null);
-  }
 
-  if(this._bufferSize[EZ3.Buffer.BITANGENT]) {
+  }else if(id === EZ3.Buffer.BITANGENT){
+
     this._buffer[EZ3.Buffer.BITANGENT] = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, this._buffer[EZ3.Buffer.BITANGENT]);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this._bufferData[EZ3.Buffer.BITANGENT]), gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), hint);
     gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+  }else if(id == EZ3.Buffer.INDEX){
+
+    this._buffer[EZ3.Buffer.INDEX] = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this._buffer[EZ3.Buffer.INDEX]);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(data), hint);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+
   }
 
 };
