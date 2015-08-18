4f62daad5e0e3891cae17eb9f97cf92f8d46fdeb
CARLOS ZAPATA <carlos.zapata.conforto@gmail.com>
2015-08-17 21:06:08 -0430

    Updated Mesh.js

--- src/objects/Mesh.js
+++ src/objects/Mesh.js
@@ -11,42 +11,46 @@ EZ3.Mesh.prototype = Object.create(EZ3.Entity.prototype);
 EZ3.Mesh.prototype.constructor = EZ3.Mesh;
 
 EZ3.Mesh.prototype.init = function(gl) {
-  var hint = (this.dynamic) ? gl.DYNAMIC_DRAW : gl.STATIC_DRAW;
+  if(this.geometry){
+    var hint = (this.dynamic) ? gl.DYNAMIC_DRAW : gl.STATIC_DRAW;
 
-  if (this.geometry.verticesNeedUpdate) {
-    this._buffer.init(gl, EZ3.Buffer.VERTEX, this.geometry.vertices.length, this.geometry.vertices, hint);
-    this.geometry.verticesNeedUpdate = false;
-  }
+    if (this.geometry.verticesNeedUpdate) {
+      this._buffer.init(gl, EZ3.Buffer.VERTEX, this.geometry.vertices.length, this.geometry.vertices, hint);
+      this.geometry.verticesNeedUpdate = false;
+    }
 
-  if (this.geometry.normalsNeedUpdate) {
-    this._buffer.init(gl, EZ3.Buffer.NORMAL, this.geometry.normals.length, this.geometry.normals, hint);
-    this.geometry.normalsNeedUpdate = false;
-  }
+    if (this.geometry.normalsNeedUpdate) {
+      this._buffer.init(gl, EZ3.Buffer.NORMAL, this.geometry.normals.length, this.geometry.normals, hint);
+      this.geometry.normalsNeedUpdate = false;
+    }
 
-  if (this.geometry.uvNeedUpdate) {
-    this._buffer.init(gl, EZ3.Buffer.UV, this.geometry.uvs.length, this.geometry.uvs, hint);
-    this.geometry.uvNeedUpdate = false;
-  }
+    if (this.geometry.uvsNeedUpdate) {
+      this._buffer.init(gl, EZ3.Buffer.UV, this.geometry.uvs.length, this.geometry.uvs, hint);
+      this.geometry.uvNeedUpdate = false;
+    }
 
-  if (this.geometry.tangentsNeedUpdate) {
-    this._buffer.init(gl, EZ3.Buffer.TANGENTS, this.geometry.tangents.length, this.geometry.tangents, hint);
-    this.geometry.tangentNeedUpdate = false;
-  }
+    if (this.geometry.tangentsNeedUpdate) {
+      this._buffer.init(gl, EZ3.Buffer.TANGENTS, this.geometry.tangents.length, this.geometry.tangents, hint);
+      this.geometry.tangentNeedUpdate = false;
+    }
 
-  if (this.geometry.bitangentsNeedUpdate) {
-    this._buffer.init(gl, EZ3.Buffer.BITANGENTS, this.geometry.bitangents.length, this.geometry.bitangents, hint);
-    this.geometry.bitangentsNeedUpdate = false;
-  }
+    if (this.geometry.bitangentsNeedUpdate) {
+      this._buffer.init(gl, EZ3.Buffer.BITANGENTS, this.geometry.bitangents.length, this.geometry.bitangents, hint);
+      this.geometry.bitangentsNeedUpdate = false;
+    }
 
-  if (this.geometry.indicesNeedUpdate) {
-    this._buffer.init(gl, EZ3.Buffer.INDEX, this.geometry.indices.length, this.geometry.indices, hint);
-    this.geometry.indicesNeedUpdate = false;
+    if (this.geometry.indicesNeedUpdate) {
+      this._buffer.init(gl, EZ3.Buffer.INDEX, this.geometry.indices.length, this.geometry.indices, hint);
+      this.geometry.indicesNeedUpdate = false;
+    }
   }
 };
 
 EZ3.Mesh.prototype.draw = function(gl) {
-  if (this.geometry.indices.length)
-    this._buffer.draw(gl, EZ3.ELEMENTS_DRAW, this.geometry.indices.length);
-  else
-    this._buffer.draw(gl, EZ3.ARRAYS_DRAW, this.geometry.vertices.length);
+  if(this.geometry){
+    if (this.geometry.indices.length)
+      this._buffer.draw(gl, EZ3.ELEMENTS_DRAW, this.geometry.indices.length);
+    else
+      this._buffer.draw(gl, EZ3.ARRAYS_DRAW, this.geometry.vertices.length);
+  }
 };
