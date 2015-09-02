e1c3abb081b9409e9706cb4d774bdfbadbb4d28d
CARLOS ZAPATA <carlos.zapata.conforto@gmail.com>
2015-09-02 09:06:05 -0430

    Updated Buffer.js

--- src/render/Buffer.js
+++ src/render/Buffer.js
@@ -22,65 +22,35 @@ Object.defineProperty(EZ3.Buffer.prototype, "data", {
   }
 });
 
-EZ3.Buffer.prototype.setup = function(gl, bufferType, layout, unitLength, dataType, normalized, stride, pointer) {
+EZ3.Buffer.prototype.setup = function(gl, bufferType, layout, unitLength, dataType, normalized, stride, offset) {
   gl.bindBuffer(bufferType, this._id);
   if(layout !== undefined) {
     gl.enableVertexAttribArray(layout);
-    gl.vertexAttribPointer(layout, unitLength, dataType, normalized, stride, pointer);
+    gl.vertexAttribPointer(layout, unitLength, dataType, normalized, stride, offset);
   }
 };
 
-EZ3.Buffer.prototype.update = function(gl, bufferType, dataType, data, hint) {
-  if (data && data.length) {
-
-    this._data = data;
-
-    if (hint === gl.STATIC_DRAW) {
-
-      if (!this._id) {
-
-        this._id = gl.createBuffer();
-
-        gl.bindBuffer(bufferType, this._id);
-
-        if(dataType === gl.FLOAT)
-          gl.bufferData(bufferType, new Float32Array(data), gl.STATIC_DRAW);
-        else
-          gl.bufferData(bufferType, new Uint16Array(data), gl.STATIC_DRAW);
-
-        gl.bindBuffer(bufferType, null);
-
-      }
-
-    } else if (hint === gl.DYNAMIC_DRAW) {
-
-      if (!this._id) {
-
-        this._id = gl.createBuffer();
-
-        gl.bindBuffer(bufferType, this._id);
-
-        if(dataType === gl.FLOAT)
-          gl.bufferData(bufferType, new Float32Array(data), gl.DYNAMIC_DRAW);
-        else
-          gl.bufferData(bufferType, new Uint16Array(data), gl.DYNAMIC_DRAW);
-
-        gl.bindBuffer(bufferType, null);
-
-      } else {
-
-        gl.bindBuffer(bufferType, this._id);
-
-        if(dataType === gl.FLOAT)
-          gl.bufferSubData(bufferType, 0, new Float32Array(data));
-        else
-          gl.bufferSubData(bufferType, 0, new Uint16Array(data));
-
-        gl.bindBuffer(bufferType, null);
-
-      }
+EZ3.Buffer.prototype.update = function(gl, bufferType, dataType, data, dynamic) {
+  if (dynamic) {
+    if (!this._id) {
+      this._id = gl.createBuffer();
+      gl.bindBuffer(bufferType, this._id);
+      gl.bufferData(bufferType, (dataType === gl.FLOAT) ? new Float32Array(data) : new Uint16Array(data), gl.DYNAMIC_DRAW);
+      gl.bindBuffer(bufferType, null);
+    } else {
+      gl.bindBuffer(bufferType, this._id);
+      gl.bufferSubData(bufferType, 0, (dataType === gl.FLOAT) ? new Float32Array(data) : new Uint16Array(data));
+      gl.bindBuffer(bufferType, null);
+    }
+  } else {
+    if (!this._id) {
+      this._id = gl.createBuffer();
+      gl.bindBuffer(bufferType, this._id);
+      gl.bufferData(bufferType, (dataType === gl.FLOAT) ? new Float32Array(data) : new Uint16Array(data), gl.STATIC_DRAW);
+      gl.bindBuffer(bufferType, null);
     }
   }
+  this._data = data;
 };
 
 EZ3.Buffer.UV_LENGTH = 2;
