d367845fcdfc32f330998b55e5d5098f9cef0bc7
CARLOS ZAPATA <carlos.zapata.conforto@gmail.com>
2015-09-01 22:10:00 -0430

    Updated Lights

--- src/lights/Light.js
+++ src/lights/Light.js
@@ -6,18 +6,7 @@ EZ3.Light = function() {
   EZ3.Entity.call(this);
 
   this.mesh = null;
-  this.linearAttenuation = null;
-  this.constantAttenuation = null;
-  this.quadraticAttenuation = null;
 };
 
-EZ3.Light.prototype._setup = function(config) {
-  this.mesh = config.mesh || null;
-  this.linearAttenuation = config.attenuation[EZ3.Light.LINEAR_ATTENUATION] || 0.2;
-  this.constantAttenuation = config.attenuation[EZ3.Light.CONSTANT_ATTENUATION] || 0.3;
-  this.quadraticAttenuation = config.attenuation[EZ3.Light.QUADRATIC_ATTENUATION] || 0.5;
-};
-
-EZ3.Light.LINEAR_ATTENUATION = 0;
-EZ3.Light.CONSTANT_ATTENUATION = 1;
-EZ3.Light.QUADRATIC_ATTENUATION = 2;
+EZ3.Light.prototype = Object.create(EZ3.Entity.prototype);
+EZ3.Light.prototype.constructor = EZ3.Light;
