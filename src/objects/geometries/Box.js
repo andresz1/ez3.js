1ab28a83841703bc63721ca0ce4052fe8fb80341
CZC91 <karlosz.91@gmail.com>
2015-09-10 11:50:41 -0430

    Updated Geometries

--- src/objects/geometries/Box.js
+++ src/objects/geometries/Box.js
@@ -8,69 +8,88 @@ EZ3.Box = function(resolution) {
 
   this._resolution = resolution;
 
-  var that = this;
-
-  function _create () {
-    var halfWidth, halfHeight, halfDepth;
-    var vertices, indices, normals;
-
-    halfWidth  = that.resolution.x * 0.5;
-    halfHeight = that.resolution.y * 0.5;
-    halfDepth  = that.resolution.z * 0.5;
-
-    vertices = [
-      +halfWidth, +halfHeight, +halfDepth,
-      -halfWidth, +halfHeight, +halfDepth,
-      -halfWidth, -halfHeight, +halfDepth,
-      +halfWidth, -halfHeight, +halfDepth,
-      +halfWidth, -halfHeight, -halfDepth,
-      -halfWidth, -halfHeight, -halfDepth,
-      -halfWidth, +halfHeight, -halfDepth,
-      +halfWidth, +halfHeight, -halfDepth
-    ];
-
-    indices = [
-      0, 1 ,2,
-      0, 2, 3,
-      7, 4, 5,
-      7, 5, 6,
-      6, 5, 2,
-      6, 2, 1,
-      7, 0, 3,
-      7, 3, 4,
-      7, 6, 1,
-      7, 1, 0,
-      3, 2, 5,
-      3, 5, 4
-    ];
-
-    normals = that.calculateNormals(indices, vertices);
-
-    that.indices = new EZ3.GeometryArray({
-      data: indices
-    });
-
-    that.normals = new EZ3.GeometryArray({
-      data: normals
-    });
-
-    that.vertices = new EZ3.GeometryArray({
-      data: vertices
-    });
-  }
-
-  _create();
+  this.update(resolution);
 };
 
 EZ3.Box.prototype = Object.create(EZ3.Geometry.prototype);
 EZ3.Box.prototype.constructor = EZ3.Box;
+
+EZ3.Box.prototype.update = function(resolution) {
+  var halfWidth, halfHeight, halfDepth;
+  var vertices, indices, normals;
+
+  halfWidth  = this.resolution.x * 0.5;
+  halfHeight = this.resolution.y * 0.5;
+  halfDepth  = this.resolution.z * 0.5;
+
+  vertices = [
+    +halfWidth, +halfHeight, +halfDepth,
+    -halfWidth, +halfHeight, +halfDepth,
+    -halfWidth, -halfHeight, +halfDepth,
+    +halfWidth, -halfHeight, +halfDepth,
+    +halfWidth, -halfHeight, -halfDepth,
+    -halfWidth, -halfHeight, -halfDepth,
+    -halfWidth, +halfHeight, -halfDepth,
+    +halfWidth, +halfHeight, -halfDepth
+  ];
+
+  indices = [
+    0, 1 ,2,
+    0, 2, 3,
+    7, 4, 5,
+    7, 5, 6,
+    6, 5, 2,
+    6, 2, 1,
+    7, 0, 3,
+    7, 3, 4,
+    7, 6, 1,
+    7, 1, 0,
+    3, 2, 5,
+    3, 5, 4
+  ];
+
+  normals = this.calculateNormals(indices, vertices);
+
+  if(!this.indices) {
+    this.indices = new EZ3.GeometryArray({
+      data: indices
+    });
+  } else {
+    this.indices.clear();
+    this.indices.update({
+      data: indices
+    });
+  }
+
+  if(!this.normals) {
+    this.normals = new EZ3.GeometryArray({
+      data: normals
+    });
+  } else {
+    this.normals.clear();
+    this.normals.update({
+      data: normals
+    });
+  }
+
+  if(!this.vertices) {
+    this.vertices = new EZ3.GeometryArray({
+      data: vertices
+    });
+  } else {
+    this.vertices.clear();
+    this.vertices.update({
+      data: vertices
+    });
+  }
+};
 
 Object.defineProperty(EZ3.Box.prototype, 'resolution', {
   get: function(){
     return this._resolution;
   },
   set: function(resolution) {
-    this._resolution.x = resolution.x;
-    this._resolution.y = resolution.y;
+    if(resolution instanceof EZ3.Vector2)
+      this._resolution.copy(resolution);
   }
 });
--- src/objects/geometries/Torus.js
+++ src/objects/geometries/Torus.js
@@ -9,103 +9,165 @@ EZ3.Torus = function(radiuses, resolution) {
   this._radiuses = radiuses;
   this._resolution = resolution;
 
-  var that = this;
-
-  function _create() {
-    var u, v;
-    var result;
-    var rho, phi;
-    var vertex, normal;
-    var cosS, cosR, sinS, sinR;
-    var vertices, normals, uvs, indices;
-    var s, r;
-
-    vertex = new EZ3.Vector3();
-    normal = new EZ3.Vector3();
-
-    uvs = [];
-    indices = [];
-    normals = [];
-    vertices = [];
-
-    for (s = 0; s < that.resolution.x; ++s) {
-      for (r = 0; r < that.resolution.y; ++r) {
-        u = s / (that.resolution.x - 1);
-        v = r / (that.resolution.y - 1);
-
-        rho = EZ3.DOUBLE_PI * u;
-        phi = EZ3.DOUBLE_PI * v;
-
-        cosS = Math.cos(rho);
-        cosR = Math.cos(phi);
-        sinS = Math.sin(rho);
-        sinR = Math.sin(phi);
-
-        vertex.x = (that.radiuses.x + that.radiuses.y * cosR) * cosS;
-        vertex.y = (that.radiuses.y * sinR);
-        vertex.z = (that.radiuses.x + that.radiuses.y * cosR) * sinS;
-
-        normal.x = vertex.x - that.radiuses.x * cosS;
-        normal.y = vertex.y;
-        normal.z = vertex.z - that.radiuses.x * sinS;
-
-        normal.normalize();
-
-        uvs.push(u);
-        uvs.push(v);
-
-        normals.push(normal.x);
-        normals.push(normal.y);
-        normals.push(normal.z);
-
-        vertices.push(vertex.x);
-        vertices.push(vertex.y);
-        vertices.push(vertex.z);
-      }
-    }
-
-    for (s = 0; s < that.resolution.x - 1; ++s) {
-      for (r = 0; r < that.resolution.y - 1; ++r) {
-        indices.push((s + 0) * that.resolution.y + (r + 0));
-        indices.push((s + 0) * that.resolution.y + (r + 1));
-        indices.push((s + 1) * that.resolution.y + (r + 1));
-
-        indices.push((s + 0) * that.resolution.y + (r + 0));
-        indices.push((s + 1) * that.resolution.y + (r + 1));
-        indices.push((s + 1) * that.resolution.y + (r + 0));
-      }
-    }
-
-    that.uvs = new EZ3.GeometryArray({
-      data: uvs
-    });
-
-    that.indices = new EZ3.GeometryArray({
-      data: indices
-    });
-
-    that.normals = new EZ3.GeometryArray({
-      data: normals
-    });
-
-    that.vertices = new EZ3.GeometryArray({
-      data: vertices
-    });
-  }
-
-  _create();
+  this.update();
 };
 
 EZ3.Torus.prototype = Object.create(EZ3.Geometry.prototype);
 EZ3.Torus.prototype.constructor = EZ3.Torus;
+
+EZ3.Torus.prototype.update = function() {
+  var u, v;
+  var result;
+  var rho, phi;
+  var vertex, normal;
+  var cosS, cosR, sinS, sinR;
+  var vertices, normals, uvs, indices;
+  var s, r;
+
+  vertex = new EZ3.Vector3();
+  normal = new EZ3.Vector3();
+
+  uvs = [];
+  indices = [];
+  normals = [];
+  vertices = [];
+
+  for (s = 0; s < this.resolution.x; ++s) {
+    for (r = 0; r < this.resolution.y; ++r) {
+      u = s / (this.resolution.x - 1);
+      v = r / (this.resolution.y - 1);
+
+      rho = EZ3.DOUBLE_PI * u;
+      phi = EZ3.DOUBLE_PI * v;
+
+      cosS = Math.cos(rho);
+      cosR = Math.cos(phi);
+      sinS = Math.sin(rho);
+      sinR = Math.sin(phi);
+
+      vertex.x = (this.radiuses.x + this.radiuses.y * cosR) * cosS;
+      vertex.y = (this.radiuses.y * sinR);
+      vertex.z = (this.radiuses.x + this.radiuses.y * cosR) * sinS;
+
+      normal.x = vertex.x - this.radiuses.x * cosS;
+      normal.y = vertex.y;
+      normal.z = vertex.z - this.radiuses.x * sinS;
+
+      normal.normalize();
+
+      uvs.push(u);
+      uvs.push(v);
+
+      normals.push(normal.x);
+      normals.push(normal.y);
+      normals.push(normal.z);
+
+      vertices.push(vertex.x);
+      vertices.push(vertex.y);
+      vertices.push(vertex.z);
+    }
+  }
+
+  for (s = 0; s < this.resolution.x - 1; ++s) {
+    for (r = 0; r < this.resolution.y - 1; ++r) {
+      indices.push((s + 0) * this.resolution.y + (r + 0));
+      indices.push((s + 0) * this.resolution.y + (r + 1));
+      indices.push((s + 1) * this.resolution.y + (r + 1));
+
+      indices.push((s + 0) * this.resolution.y + (r + 0));
+      indices.push((s + 1) * this.resolution.y + (r + 1));
+      indices.push((s + 1) * this.resolution.y + (r + 0));
+    }
+  }
+
+  output = this.calculateTangentsAndBitangents(indices, uvs, normals, vertices);
+
+  if(!this.uvs) {
+    this.uvs = new EZ3.GeometryArray({
+      data: uvs,
+      dynamic: true
+    });
+  } else {
+    this.uvs.clear();
+    this.uvs.update({
+      data: uvs,
+      dynamic: true
+    });
+  }
+
+  if(!this.indices) {
+    this.indices = new EZ3.GeometryArray({
+      data: indices,
+      dynamic: true
+    });
+  } else {
+    this.indices.clear();
+    this.indices.update({
+      data: indices,
+      dynamic: true
+    });
+  }
+
+  if(!this.normals) {
+    this.normals = new EZ3.GeometryArray({
+      data: normals,
+      dynamic: true
+    });
+  } else {
+    this.normals.clear();
+    this.normals.update({
+      data: normals,
+      dynamic: true
+    });
+  }
+
+  if(!this.vertices) {
+    this.vertices = new EZ3.GeometryArray({
+      data: vertices,
+      dynamic: true
+    });
+  } else {
+    this.vertices.clear();
+    this.vertices.update({
+      data: vertices,
+      dynamic: true
+    });
+  }
+
+  if(!this.tangents) {
+    this.tangents = new EZ3.GeometryArray({
+      data: output.tangents,
+      dynamic: true
+    });
+  } else {
+    this.tangents.clear();
+    this.tangents.update({
+      data: output.tangents,
+      dynamic: true
+    });
+  }
+
+  if(!this.bitangents) {
+    this.bitangents = new EZ3.GeometryArray({
+      data: output.bitangents,
+      dynamic: true
+    });
+  } else {
+    this.bitangents.clear();
+    this.bitangents.update({
+      data: output.bitangents,
+      dynamic: true
+    });
+  }
+};
 
 Object.defineProperty(EZ3.Torus.prototype, 'radiuses', {
   get: function() {
     return this._radiuses;
   },
   set: function(radiuses) {
-    this._radiuses.x = radiuses.x;
-    this._radiuses.y = radiuses.y;
+    if(radiuses instanceof EZ3.Vector2)
+      this._radiuses.copy(radiuses);
   }
 });
 
@@ -114,7 +176,7 @@ Object.defineProperty(EZ3.Torus.prototype, 'resolution', {
     return this._resolution;
   },
   set: function(resolution) {
-    this._resolution.x = resolution.x;
-    this._resolution.y = resolution.y;
+    if(resolution instanceof EZ3.Vector2)
+      this._resolution.copy(resolution);
   }
 });
