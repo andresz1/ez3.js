63c133aa047cfafeeec12ced313d8d577c26ef2d
CARLOS ZAPATA <carlos.zapata.conforto@gmail.com>
2015-09-17 16:01:17 -0430

    Updated shdrsconcat.js

--- tasks/shdrsconcat.js
+++ tasks/shdrsconcat.js
@@ -9,7 +9,7 @@ module.exports = {
         '',
         'EZ3.ShaderLibrary = function() {',
         '  this.mesh = {};',
-        '}',
+        '};',
         '',
         'EZ3.ShaderLibrary = new EZ3.ShaderLibrary();',
         '\n'
