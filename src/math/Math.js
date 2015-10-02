6a15d1ae42b3b3b30203bf303f5f6c79bf1b22b5
CARLOS ZAPATA <carlos.zapata.conforto@gmail.com>
2015-10-02 15:13:08 -0430

    Updated Math.js

--- src/math/Math.js
+++ src/math/Math.js
@@ -6,6 +6,8 @@ EZ3.Math = function() {
   this.PI = Math.PI;
   this.HALF_PI = this.PI * 0.5;
   this.DOUBLE_PI = 2.0 * this.PI;
+  this.MAX_UINT = Math.pow(2, 32) - 1;
+  this.MAX_USHORT = Math.pow(2, 16) - 1;
 };
 
 EZ3.Math = new EZ3.Math();
