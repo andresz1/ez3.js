944c4f5c5617764743a59b84c21775f77f51c13b
CARLOS ZAPATA <carlos.zapata.conforto@gmail.com>
2015-10-23 21:34:50 -0430

    Updated Screen.js

--- src/core/Screen.js
+++ src/core/Screen.js
@@ -7,42 +7,36 @@ EZ3.Screen = function(id, position, size) {
   this.position = position;
   this.size = size;
   this.scene = new EZ3.Scene();
-  this.camera = new EZ3.FreeCamera(
-    new EZ3.Vector3(50,50,50),
+  this.camera = new EZ3.TargetCamera(
+    new EZ3.Vector3(30, 30, 30),
     new EZ3.Vector3(0, 0, 0),
     new EZ3.Vector3(0, 1, 0)
   );
+  this.camera.aspectRatio = 800 / 600;
+  this.date = new Date();
+  this.deltaTime = 0;
+  this.previousTime = 0;
+  this.pressed = false;
 };
 
-EZ3.Screen.prototype.onKeyPress = function() {
-};
+EZ3.Screen.prototype.onKeyPress = function() {};
 
-EZ3.Screen.prototype.onKeyDown = function() {
-};
+EZ3.Screen.prototype.onKeyDown = function() {};
 
-EZ3.Screen.prototype.onKeyUp = function() {
-};
+EZ3.Screen.prototype.onKeyUp = function() {};
 
-EZ3.Screen.prototype.onMousePress = function() {
-};
+EZ3.Screen.prototype.onMousePress = function() {};
 
-EZ3.Screen.prototype.onMouseMove = function() {
-};
+EZ3.Screen.prototype.onMouseMove = function() {};
 
-EZ3.Screen.prototype.onMouseUp = function() {
-};
+EZ3.Screen.prototype.onMouseUp = function() {};
 
-EZ3.Screen.prototype.onTouchPress = function() {
-};
+EZ3.Screen.prototype.onTouchPress = function() {};
 
-EZ3.Screen.prototype.onTouchMove = function() {
-};
+EZ3.Screen.prototype.onTouchMove = function() {};
 
-EZ3.Screen.prototype.onTouchUp = function() {
-};
+EZ3.Screen.prototype.onTouchUp = function() {};
 
-EZ3.Screen.prototype.create = function() {
-};
+EZ3.Screen.prototype.create = function() {};
 
-EZ3.Screen.prototype.update = function() {
-};
+EZ3.Screen.prototype.update = function() {};
