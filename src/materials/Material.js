dcb7b9f757a4c38b8c31b7bee843185af1272900
CZC91 <karlosz.91@gmail.com>
2015-10-28 11:47:44 -0430

    Updated Material.js with state

--- src/materials/Material.js
+++ src/materials/Material.js
@@ -8,22 +8,53 @@ EZ3.Material = function(name) {
   this.program = null;
   this.fill = EZ3.MeshMaterial.WIREFRAME;
   this.transparent = false;
+  this.faceCulling = true;
   this.backFaceCulling = true;
+  this.frontFaceCulling = false;
   this.depthTest = true;
 };
 
-EZ3.Material.prototype.updateStates = function(gl) {
+EZ3.Material.prototype.updateStates = function(gl, state) {
   if (this.depthTest) {
-    gl.enable(gl.DEPTH_TEST);
-    gl.depthFunc(gl.LEQUAL);
-  } else
-    gl.disable(gl.DEPTH_TEST);
+    if (!state.enabled[EZ3.RendererState.DEPTH_TEST]) {
+      gl.enable(gl.DEPTH_TEST);
+      state.enabled[EZ3.RendererState.DEPTH_TEST] = true;
+    }
+    if (state.enabledValue[EZ3.RendererState.DEPTH_TEST] !== gl.LEQUAL) {
+      gl.depthFunc(gl.LEQUAL);
+      state.enabledValue[EZ3.RendererState.DEPTH_TEST] = gl.LEQUAL;
+    }
+  } else {
+    if (state.enabled[EZ3.RendererState.DEPTH_TEST]) {
+      gl.disable(gl.DEPTH_TEST);
+      state.enabled[EZ3.RendererState.DEPTH_TEST] = false;
+      state.enabledValue[EZ3.RendererState.DEPTH_TEST] = null;
+    }
+  }
 
-  if (this.backFaceCulling) {
-    gl.enable(gl.CULL_FACE);
-    gl.cullFace(gl.BACK);
-  } else
-    gl.disable(gl.CULL_FACE);
+  if (this.faceCulling) {
+    if (!state.enabled[EZ3.RendererState.CULL_FACE]) {
+      gl.enable(gl.CULL_FACE);
+      state.enabled[EZ3.RendererState.CULL_FACE] = true;
+    }
+    if (this.backFaceCulling) {
+      if(state.enabledValue[EZ3.RendererState.CULL_FACE] !== gl.BACK) {
+        gl.cullFace(gl.BACK);
+        state.enabledValue[EZ3.RendererState.CULL_FACE] = gl.BACK;
+      }
+    } else if(this.frontFaceCulling) {
+      if(state.enabledValue[EZ3.RendererState.CULL_FACE] !== gl.FRONT) {
+        gl.cullFace(gl.FRONT);
+        state.enabledValue[EZ3.RendererState.CULL_FACE] = gl.FRONT;
+      }
+    }
+  } else {
+    if (state.enabled[EZ3.RendererState.CULL_FACE]) {
+      gl.disable(gl.CULL_FACE);
+      state.enabled[EZ3.RendererState.CULL_FACE] = false;
+      state.enabledValue[EZ3.RendererState.CULL_FACE] = null;
+    }
+  }
 };
 
 EZ3.Material.SOLID = 0;
