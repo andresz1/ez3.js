07d6667a5d95dde17840890ea30525b267784a6a
CZC91 <karlosz.91@gmail.com>
2015-10-28 11:48:27 -0430

    Updated RendererState.js

--- src/render/RendererState.js
+++ src/render/RendererState.js
@@ -2,64 +2,18 @@
  * @class RendererState
  */
 
-EZ3.RendererState = function(gl) {
-  this.gl = gl;
+EZ3.RendererState = function() {
+  this.enabled = {};
+  this.enabledValue = {};
   this.program = {};
   this.extension = {};
-  this.capability = {};
   this.attribute = {};
-
-  this._init();
 };
 
 EZ3.RendererState.prototype.constructor = EZ3.RendererState;
 
-EZ3.RendererState.prototype._init = function() {
-  this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
-  this.gl.clear(this.context.COLOR_BUFFER_BIT | this.context.DEPTH_BUFFER_BIT);
-};
-
-EZ3.RendererState.prototype.enable = function(id) {
-  if(!this.capability[name]) {
-    this.capability[name] = true;
-    this.gl.enable(id);
-  }
-};
-
-EZ3.RendererState.prototype.disable = function(id) {
-  if(this.capability[name]) {
-    this.capability[name] = false;
-    this.gl.disable(id);
-  }
-};
-
-EZ3.RendererState.prototype.enableAttribute = function(attribute) {
-  if(!this.attribute[attribute]) {
-    this.attribute[attribute] = true;
-    this.gl.enableVertexAttribArray(attribute);
-  }
-};
-
-EZ3.RendererState.prototype.disableAttribute = function(attribute) {
-  if(this.attribute[attribute]) {
-    this.attribute[attribute] = false;
-    this.gl.disableVertexAttribArray(attribute);
-  }
-};
-
-EZ3.RendererState.prototype.activeTexture = function(unit) {
-
-};
-
-EZ3.RendererState.prototype.bindTexture = function(target, id) {
-
-};
-
-EZ3.RendererState.prototype.setBlending = function(mode) {
-
-};
-
-EZ3.NO_BLENDING = 0;
-EZ3.ADDITIVE_BLENDING = 1;
-EZ3.SUBSTRACTIVE_BLENDING = 2;
-EZ3
+EZ3.RendererState.CULL_FACE = 0;
+EZ3.RendererState.BLENDING = 1;
+EZ3.RendererState.DEPTH_TEST = 2;
+EZ3.RendererState.BLEND_EQUATION = 3;
+EZ3.RendererState.BLEND_FUNCTION = 4;
