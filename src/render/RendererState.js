6c53e225e8220cf1821a4aa6d95226dc03c92dd3
CARLOS ZAPATA <carlos.zapata.conforto@gmail.com>
2015-10-28 21:25:36 -0430

    Updated RendererState

--- src/render/RendererState.js
+++ src/render/RendererState.js
@@ -2,18 +2,43 @@
  * @class RendererState
  */
 
-EZ3.RendererState = function() {
-  this.enabled = {};
-  this.enabledValue = {};
+EZ3.RendererState = function(gl) {
   this.program = {};
   this.extension = {};
   this.attribute = {};
+  this.capability = {};
+
+  this._initCapabilities();
+  this._initExtensions(gl);
 };
 
 EZ3.RendererState.prototype.constructor = EZ3.RendererState;
 
+EZ3.RendererState.prototype._initCapabilities = function() {
+  this.capability[EZ3.RendererState.DEPTH_TEST] = {
+    enabled: false,
+    value: null
+  };
+  this.capability[EZ3.RendererState.CULL_FACE] = {
+    enabled: false,
+    value: null
+  };
+  this.capability[EZ3.RendererState.BLENDING] = {
+    enabled: false,
+    blendEquation: null,
+    blendFunc: {
+      sfactor: null,
+      dfactor: null
+    }
+  };
+};
+
+EZ3.RendererState.prototype._initExtensions = function(gl) {
+  this.extension['OES_standard_derivatives'] = gl.getExtension('OES_standard_derivatives');
+  this.extension['OES_vertex_array_object'] = gl.getExtension('OES_vertex_array_object');
+  this.extension['OES_element_index_uint'] = gl.getExtension('OES_element_index_uint');
+};
+
 EZ3.RendererState.CULL_FACE = 0;
 EZ3.RendererState.BLENDING = 1;
 EZ3.RendererState.DEPTH_TEST = 2;
-EZ3.RendererState.BLEND_EQUATION = 3;
-EZ3.RendererState.BLEND_FUNCTION = 4;
