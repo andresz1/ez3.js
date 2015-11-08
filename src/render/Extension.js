7a49458790b6b628008063ca98c78e0397e6fbe0
CARLOS ZAPATA <carlos.zapata.conforto@gmail.com>
2015-11-08 16:18:08 -0430

    Updated Extensions.js

--- src/render/Extension.js
+++ src/render/Extension.js
@@ -6,4 +6,5 @@ EZ3.Extension = function(gl) {
   this.elementIndexUInt = gl.getExtension('OES_element_index_uint');
   this.vertexArrayObject = gl.getExtension('OES_vertex_array_object');
   this.standardDerivates = gl.getExtension('OES_standard_derivatives');
+  this.shaderLOD = gl.getExtension('EXT_shader_texture_lod');
 };
