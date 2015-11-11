8d7a769e77e70ff80991f5a747ab5c3092b7553f
CZC91 <karlosz.91@gmail.com>
2015-11-11 12:04:21 -0430

    Updated TargetTexture.js

--- src/textures/TargetTexture2D.js
+++ src/textures/TargetTexture2D.js
@@ -10,7 +10,7 @@
  EZ3.TargetTexture2D.prototype = Object.create(EZ3.Texture2D.prototype);
  EZ3.TargetTexture2D.prototype.constructor = EZ3.TargetTexture2D;
 
- EZ3.TargetTexture.prototype.attachToFramebuffer = function(gl, attachment) {
+ EZ3.TargetTexture2D.prototype.attachToFramebuffer = function(gl, attachment) {
    if(this._cache.framebufferAttachment !== attachment) {
      this._cache.framebufferAttachment = attachment;
      gl.framebufferTexture2D(gl.FRAMEBUFFER, gl[attachment], gl.TEXTURE_2D, this._id, 0);
