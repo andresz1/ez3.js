4afef6714d4e2f8e09ecd1f504e7a1de55532661
CARLOS ZAPATA <carlos.zapata.conforto@gmail.com>
2015-11-22 15:42:53 -0430

    Updated

--- src/controls/FreeControl.js
+++ src/controls/FreeControl.js
@@ -6,22 +6,36 @@
 EZ3.FreeControl = function(entity, target, up) {
   EZ3.Control.call(this, entity);
 
-  this.moveSpeed = 50.0;
-  this.rotationSpeed = 150.0;
+  this.yaw = 0.0;
+  this.pitch = 0.0;
+  this.roll = 0.0;
 
   this.right = new EZ3.Vector3();
   this.up = up || new EZ3.Vector3();
   this.target = target || new EZ3.Vector3();
   this.look = new EZ3.Vector3().sub(this.entity.position, this.target);
 
-  this._setPointOfView(this.look);
+  if(!this.look.testZero())
+    this.look.normalize();
+
+  this.rotate(
+    -EZ3.Math.toDegrees(Math.atan2(this.look.z, this.look.x) + EZ3.Math.PI),
+    EZ3.Math.toDegrees(Math.asin(this.look.y)),
+    1.0
+  );
 };
 
 EZ3.FreeControl.prototype = Object.create(EZ3.Control.prototype);
 EZ3.FreeControl.prototype.constructor = EZ3.FreeControl;
 
-EZ3.FreeControl.prototype.update = function() {
-  var matrix = new EZ3.Matrix4().yawPitchRoll(this.yaw, this.pitch, this.roll);
+EZ3.FreeControl.prototype.rotate = function(dx, dy, speed) {
+  var rotationSpeed = speed || 150.0;
+  var matrix;
+
+  this.yaw -= dx * rotationSpeed;
+  this.pitch += dy * rotationSpeed;
+
+  matrix = new EZ3.Matrix4().yawPitchRoll(this.yaw, this.pitch, this.roll);
 
   this.up = new EZ3.Vector4(0.0, 1.0, 0.0, 0.0).mulMat4(matrix).toVec3();
   this.look = new EZ3.Vector4(0.0, 0.0, 1.0, 0.0).mulMat4(matrix).toVec3();
@@ -30,11 +44,6 @@ EZ3.FreeControl.prototype.update = function() {
   this.target = new EZ3.Vector3().add(this.entity.position, this.look);
 
   this.entity.lookAt(this.target, this.up);
-};
-
-EZ3.FreeControl.prototype.rotate = function(dx, dy) {
-  this.yaw -= dx * this.rotationSpeed;
-  this.pitch += dy * this.rotationSpeed;
 };
 
 EZ3.FreeControl.prototype.lift = function(speed) {
--- src/controls/TargetControl.js
+++ src/controls/TargetControl.js
@@ -6,23 +6,40 @@
 EZ3.TargetControl = function(entity, target, up) {
   EZ3.Control.call(this, entity);
 
+  this.yaw = 0.0;
+  this.pitch = 0.0;
+  this.roll = 0.0;
+
   this.moveSpeed = 50.0;
-  this.rotationSpeed = 150.0;
 
   this.right = new EZ3.Vector3();
   this.up = up || new EZ3.Vector3();
   this.target = target || new EZ3.Vector3();
   this.look = new EZ3.Vector3().sub(this.entity.position, this.target);
 
-  this._setPointOfView(this.look);
+  if(!this.look.testZero())
+    this.look.normalize();
+
+  this.rotate(
+    -EZ3.Math.toDegrees(Math.atan2(this.look.z, this.look.x) + EZ3.Math.PI),
+    EZ3.Math.toDegrees(Math.asin(this.look.y)),
+    1.0
+  );
 };
 
 EZ3.TargetControl.prototype = Object.create(EZ3.Control.prototype);
 EZ3.TargetControl.prototype.constructor = EZ3.TargetControl;
 
-EZ3.TargetControl.prototype.update = function() {
-  var matrix = new EZ3.Matrix4().yawPitchRoll(this.yaw, this.pitch, this.roll);
-  var vector = new EZ3.Vector4(0, 0, -1, 0).mulMat4(matrix).toVec3();
+EZ3.TargetControl.prototype.rotate = function(dx, dy, speed) {
+  var rotationSpeed = speed || 150.0;
+  var matrix;
+  var vector;
+
+  this.yaw -= dx * rotationSpeed;
+  this.pitch += dy * rotationSpeed;
+
+  matrix = new EZ3.Matrix4().yawPitchRoll(this.yaw, this.pitch, this.roll);
+  vector = new EZ3.Vector4(0, 0, -1, 0).mulMat4(matrix).toVec3();
 
   this.distance = new EZ3.Vector3().sub(this.entity.position, this.target).length();
   this.distance = Math.max(1, this.distance);
@@ -37,20 +54,16 @@ EZ3.TargetControl.prototype.update = function() {
   this.entity.lookAt(this.target, this.up);
 };
 
-EZ3.TargetControl.prototype.rotate = function(dx, dy) {
-  this.yaw -= dx * this.rotationSpeed;
-  this.pitch += dy * this.rotationSpeed;
-};
-
-EZ3.TargetControl.prototype.pan = function(dx, dy) {
+EZ3.TargetControl.prototype.pan = function(dx, dy, speed) {
+  var moveSpeed = speed || 50.0;
   var rx;
   var ry;
   var up;
   var right;
   var vector;
 
-  rx = dx * this.moveSpeed;
-  ry = -dy * this.moveSpeed;
+  rx = dx * moveSpeed;
+  ry = -dy * moveSpeed;
 
   right = new EZ3.Vector3().copy(this.right).scale(rx);
   up = new EZ3.Vector3().copy(this.up).scale(ry);
