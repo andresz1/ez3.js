686f1d64292dddf1b441cfc2f372a49dc65ab58e
CARLOS ZAPATA <carlos.zapata.conforto@gmail.com>
2015-11-24 00:08:49 -0430

    Updated

--- src/renderbuffers/DepthRenderbuffer.js
+++ src/renderbuffers/DepthRenderbuffer.js
@@ -3,17 +3,17 @@
  * @extends Renderbuffer
  */
 
- EZ3.DepthRenderbuffer = function(size) {
-   EZ3.Renderbuffer.call(this, size);
+ EZ3.DepthRenderbuffer = function(size, storage) {
+   EZ3.Renderbuffer.call(this, size, storage);
  };
 
  EZ3.DepthRenderbuffer.prototype = Object.create(EZ3.Renderbuffer.prototype);
  EZ3.DepthRenderbuffer.prototype.constructor = EZ3.DepthRenderbuffer;
 
  EZ3.DepthRenderbuffer.prototype.update = function(gl) {
-   EZ3.Renderbuffer.prototype.update.call(this, gl, 'DEPTH_COMPONENT16');
+   EZ3.Renderbuffer.prototype.update.call(this, gl);
  };
 
- EZ3.DepthRenderbuffer.prototype.attachToFramebuffer = function(gl) {
-   EZ3.Renderbuffer.prototype.attachToFramebuffer.call(this, gl, 'DEPTH_ATTACHMENT');
+ EZ3.DepthRenderbuffer.prototype.attachToFramebuffer = function(gl, attachment) {
+   EZ3.Renderbuffer.prototype.attachToFramebuffer.call(this, gl, attachment);
  };
--- src/renderbuffers/Renderbuffer.js
+++ src/renderbuffers/Renderbuffer.js
@@ -2,10 +2,11 @@
  * @class Renderbuffer
  */
 
-EZ3.Renderbuffer = function(size) {
+EZ3.Renderbuffer = function(size, storage) {
   this._id = null;
   this._cache = {};
   this.size = size;
+  this.storage = storage;
 };
 
 EZ3.Renderbuffer.prototype.constructor = EZ3.Renderbuffer;
@@ -17,16 +18,16 @@ EZ3.Renderbuffer.prototype.bind = function(gl) {
   gl.bindRenderbuffer(gl.RENDERBUFFER, this._id);
 };
 
-EZ3.Renderbuffer.prototype.update = function(gl, storage) {
-  if(this._cache.storage !== storage) {
-    this._cache.storage = storage;
-    gl.renderbufferStorage(gl.RENDERBUFFER, gl[storage], this.size.x, this.size.y);
+EZ3.Renderbuffer.prototype.update = function(gl) {
+  if(this._cache.storage !== this.storage) {
+    gl.renderbufferStorage(gl.RENDERBUFFER, gl[this.storage], this.size.x, this.size.y);
+    this._cache.storage = this.storage.toString();
   }
 };
 
 EZ3.Renderbuffer.prototype.attachToFramebuffer = function(gl, attachment) {
   if(this._cache.attachment !== attachment) {
-    this._cache.attachment = attachment;
     gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl[attachment], gl.RENDERBUFFER, this._id);
+    this._cache.attachment = attachment;
   }
 };
