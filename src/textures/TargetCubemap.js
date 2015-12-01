92f953ce98deb2db640f2155f9c0b24c43cb03c5
CARLOS ZAPATA <carlos.zapata.conforto@gmail.com>
2015-12-01 08:27:35 -0430

    Updated

--- src/textures/TargetCubemap.js
+++ src/textures/TargetCubemap.js
@@ -3,21 +3,29 @@
  * @extends Cubemap
  */
 
- EZ3.TargetCubemap = function(resolution, target) {
+ EZ3.TargetCubemap = function(size, target, attachment) {
    EZ3.Cubemap.call(this,
-     new EZ3.Image(resolution.x, resolution.y, target, null),
-     new EZ3.Image(resolution.x, resolution.y, target, null),
-     new EZ3.Image(resolution.x, resolution.y, target, null),
-     new EZ3.Image(resolution.x, resolution.y, target, null),
-     new EZ3.Image(resolution.x, resolution.y, target, null),
-     new EZ3.Image(resolution.x, resolution.y, target, null),
+     new EZ3.Image(size.x, size.y, target, null),
+     new EZ3.Image(size.x, size.y, target, null),
+     new EZ3.Image(size.x, size.y, target, null),
+     new EZ3.Image(size.x, size.y, target, null),
+     new EZ3.Image(size.x, size.y, target, null),
+     new EZ3.Image(size.x, size.y, target, null),
      false
    );
+
+   this.attachment = attachment;
  };
 
  EZ3.TargetCubemap.prototype = Object.create(EZ3.Cubemap.prototype);
  EZ3.TargetCubemap.prototype.constructor = EZ3.TargetCubemap;
 
- EZ3.TargetCubemap.prototype.attachToFramebuffer = function(gl, attachment) {
-   // TODO
+ EZ3.TargetCubemap.prototype.attach = function(gl, face) {
+   var index = face || 0;
+
+   if(this._cache.attachment !== this.attachment || this._cache.target !== index) {
+     this._cache.target = index;
+     this._cache.attachment = this.attachment;
+     gl.framebufferTexture2D(gl.FRAMEBUFFER, gl[this.attachment], gl.TEXTURE_CUBE_MAP_POSITIVE_X + index, this._id, 0);
+   }
  };
--- src/textures/TargetTexture2D.js
+++ src/textures/TargetTexture2D.js
@@ -3,16 +3,18 @@
  * @extends Texture2D
  */
 
- EZ3.TargetTexture2D = function(size, format) {
+ EZ3.TargetTexture2D = function(size, format, attachment) {
    EZ3.Texture2D.call(this, new EZ3.Image(size.x, size.y, format, null), false);
+
+   this.attachment = attachment;
  };
 
  EZ3.TargetTexture2D.prototype = Object.create(EZ3.Texture2D.prototype);
  EZ3.TargetTexture2D.prototype.constructor = EZ3.TargetTexture2D;
 
- EZ3.TargetTexture2D.prototype.attachToFramebuffer = function(gl, attachment) {
-   if(this._cache.attachment!== attachment) {
-     gl.framebufferTexture2D(gl.FRAMEBUFFER, gl[attachment], gl.TEXTURE_2D, this._id, 0);
-     this._cache.attachment = attachment;
+ EZ3.TargetTexture2D.prototype.attach = function(gl) {
+   if(this._cache.attachment !== this.attachment) {
+     gl.framebufferTexture2D(gl.FRAMEBUFFER, gl[this.attachment], gl.TEXTURE_2D, this._id, 0);
+     this._cache.attachment = this.attachment.slice();
    }
  };
