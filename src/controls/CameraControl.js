bb66c0cf4b0802ba88950fc268c664ce9ec2b0cd
CZC91 <karlosz.91@gmail.com>
2015-12-03 12:07:34 -0430

    Updated

--- src/controls/CameraControl.js
+++ src/controls/CameraControl.js
@@ -6,10 +6,10 @@
 EZ3.CameraControl = function(entity, target, up) {
   EZ3.Control.call(this, entity);
 
-  this.rigth = new EZ3.Vector3();
+  this.right = new EZ3.Vector3();
   this.roll = 0;
-  this.minPitch = -Infinity;
-  this.maxPitch = Infinity;
+  this.minPitch = -EZ3.Math.HALF_PI;
+  this.maxPitch = EZ3.Math.HALF_PI;
   this.minYaw = -Infinity;
   this.maxYaw = Infinity;
 
@@ -24,12 +24,13 @@ EZ3.CameraControl.prototype.lookAt = function(target, up) {
 
   this.target = target || new EZ3.Vector3();
   this.up = up || new EZ3.Vector3(0, 1, 0);
-  this.look = new EZ3.Vector3().sub(this.entity.position, this.target);
+  this.look = new EZ3.Vector3().sub(this.target, this.entity.position).normalize();
+  this.right = new EZ3.Vector3().cross(this.look, this.up);
 
   xy = Math.sqrt(this.look.x * this.look.x + this.look.z * this.look.z);
 
-  this.yaw = Math.atan2(-this.look.x, -this.look.z);
-  this.pitch = Math.atan2(this.look.y, xy);
+  this.yaw = Math.atan2(this.look.x, this.look.z);
+  this.pitch = Math.atan2(-this.look.y, xy);
 
   this.entity.lookAt(this.target, this.up);
 };
